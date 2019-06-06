854944727b69391561ae19b6fdcf5fa057bb0696
qixiangyang <qixiangyangrm@foxmail.com>
2019-06-06 14:52:31 +0800

    algorithm update

---
+++ algorithm/chapter1/divide_and_conquer.py
@@ -0,0 +1,17 @@
+"""
+Description:
+Author:qxy
+Date: 2019-06-05 19:06
+File: divide_and_conquer 
+"""
+
+
+def quicksort(array):
+    if len(array) < 2:
+        return array
+    else:
+
+        return
+
+
+print(quicksort([1, 6, 7, 3, 2]))
