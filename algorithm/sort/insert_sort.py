4bd0adf3e8d08711c118adf41ff549e35c445b91
qixiangyang <qixiangyangrm@foxmail.com>
2019-07-03 16:37:19 +0800

    基金业协会 私募股权基金

--- algorithm/sort/insert_sort.py
+++ algorithm/sort/insert_sort.py
@@ -13,9 +13,9 @@ File: insert_sort
 def insert_sort(list_data):
 
     for i in range(len(list_data)):
-        for m in range(i, -1, -1):
-            print(m)
-
+        for m in range(i, 0, -1):
+            if list_data[m] < list_data[m-1]:
+                list_data[m], list_data[m-1] = list_data[m-1], list_data[m]
 
 list_data = [54, 26, 93, 17, 77, 31, 44, 55, 20]
 insert_sort(list_data)
