68371b5be4aeda00d6f5b26f9aee35b053f87c04
qixiangyang <qixiangyangrm@foxmail.com>
2019-07-10 09:47:33 +0800

    insert sort

--- Python/Asynchronous/Asynchronous.py
+++ Python/Asynchronous/Asynchronous.py
@@ -5,3 +5,43 @@ Date: 2019-07-09 16:18
 File: Asynchronous 
 """
 
+import asyncio
+import threading
+
+#
+# @asyncio.coroutine
+# def hello():
+#     print('Hello world! (%s)' % threading.currentThread())
+#     yield from asyncio.sleep(1)
+#     print('Hello again! (%s)' % threading.currentThread())
+#
+#
+# # 获取EventLoop:
+# loop = asyncio.get_event_loop()
+# # 执行coroutine
+# tasks = [hello(), hello()]
+# loop.run_until_complete(asyncio.wait(tasks))
+# loop.close()
+
+
+@asyncio.coroutine
+def wget(host):
+    print('wget %s...' % host)
+    connect = asyncio.open_connection(host, 80)
+    reader, writer = yield from connect
+    header = 'GET / HTTP/1.0\r\nHost: %s\r\n\r\n' % host
+    writer.write(header.encode('utf-8'))
+    yield from writer.drain()
+    while True:
+        line = yield from reader.readline()
+        if line == b'\r\n':
+            break
+        print('%s header > %s' % (host, line.decode('utf-8').rstrip()))
+        writer.close()
+
+
+loop = asyncio.get_event_loop()
+tasks = [wget(host) for host in ['www.sina.com.cn', 'www.sohu.com', 'www.163.com']]
+loop.run_until_complete(asyncio.wait(tasks))
+loop.close()
+
