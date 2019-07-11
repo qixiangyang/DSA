1897cce37357d41901f2185079ef007a03843d9e
qixiangyang <qixiangyangrm@foxmail.com>
2019-07-11 17:17:45 +0800

    Tree data Structure

--- Data_Structure/Tree/heap.py
+++ Data_Structure/Tree/heap.py
@@ -14,6 +14,11 @@ url：https://www.jianshu.com/p/d174f1862601
 # L.appendleft(0)
 #
 # print(L)
+"""
+堆的特点：
+堆有两点需要了解，一是堆是一颗完全二叉树，完全二叉树就是只有最后一层有页子节点，而且页子节点是靠左排列的；
+二是堆中的每一个节点都大于其左右子节点（大顶堆），或者堆中每一个节点都小于其左右子节点（小顶堆）。
+"""
 
 
 class Heap(object):
@@ -32,7 +37,7 @@ class Heap(object):
         :return:
         """
 
-        if index == 0 or index > len(self.data_list) -1:
+        if index == 0 or index > len(self.data_list) - 1:
             return None
         else:
             return (index - 1) >> 1
@@ -44,7 +49,56 @@ class Heap(object):
 
         self.data_list.append(data)
         index = len(self.data_list) - 1
+        print(index)
+        parent = self.get_parent_index(index)
+        # 循环，直到该元素成为堆顶，或小于父节点（对于大顶堆)
+        print(index, parent)
 
-        while self.data_list:
-            pass
+        while parent is not None and self.data_list[parent] < self.data_list[index]:
+            # 交换操作
+            self.swap(parent, index)
+            index = parent
+            parent = self.get_parent_index(parent)
+
+    def removeMax(self):
+        """
+        删除堆定元素
+        """
+        remove_data = self.data_list[0]
+        self.data_list[0] = self.data_list[-1]
+        del self.data_list[-1]
+
+        # 堆化
+        self.heapify(0)
+        return remove_data
+
+    def heapify(self, index: int):
+        """
+        从上往下堆化，从index 开始堆化操作 (大顶堆)
+        """
+        total_index = len(self.data_list) - 1
+        while True:
+            maxvalue_index = index
+            if 2*index + 1 <= total_index and self.data_list[2*index + 1] > self.data_list[maxvalue_index]:
+                maxvalue_index = 2*index + 1
+            if 2*index + 2 <= total_index and self.data_list[2*index + 2] > self.data_list[maxvalue_index]:
+                maxvalue_index = 2*index + 2
+            if maxvalue_index == index:
+                break
+            self.swap(index, maxvalue_index)
+            index = maxvalue_index
+
+
+if __name__ == "__main__":
+    myheap = Heap()
+    for i in range(10):
+        myheap.insert(i+1)
+    print('建堆:', myheap.data_list)
+
+    print("删除堆顶元素：", [myheap.removeMax() for _ in range(10)])
+
+
+
+
+
 
