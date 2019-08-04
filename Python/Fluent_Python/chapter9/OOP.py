e034f58e5496fe74cec79aa718ad9230ab9ac094
qixiangyang <qixiangyangrm@foxmail.com>
2019-08-04 17:05:55 +0800

    面向对象学习

--- Python/Fluent_Python/chapter9/OOP.py
+++ Python/Fluent_Python/chapter9/OOP.py
@@ -13,8 +13,8 @@ class Vector2d:
     typecode = 'd'
 
     def __init__(self, x, y):
-        self.x = float(x)
-        self.y = float(y)
+        self.__x = float(x)
+        self.__y = float(y)
 
     def __iter__(self):
         return (i for i in (self.x, self.y))
@@ -39,6 +39,32 @@ class Vector2d:
     def __bool__(self):
         return bool(abs(self))
 
+    def angle(self):
+        return math.atan2(self.x, self.y)
+
+    def __format__(self, format_spec=''):
+        if format_spec.endswith('p'):
+            format_spec = format_spec[:-1]
+            coords = (abs(self), self.angle())
+            outer_fmt = '<{}, {}>'
+        else:
+            coords = self
+            outer_fmt = '({}, {})'
+
+        components = (format(c, format_spec) for c in coords)
+        return outer_fmt.format(*components)
+
+    @property
+    def x(self):
+        return self.__x
+
+    @property
+    def y(self):
+        return self.__y
+
+    def __hash__(self):
+        return hash(self.x) ^ hash(self.y)
+
     @classmethod
     def frombytes(cls, octets):
         typecpde = chr(octets[0])
@@ -49,13 +75,16 @@ class Vector2d:
 v1 = Vector2d(3, 4)
 print(v1.x, v1.y)
 
-x, y = v1
-print(x, y)
-print(v1)
 
-v1_clone = eval(str(v1))
-print(v1_clone == v1)
 
-print(abs(v1))
 
-print(bool(v1))
+# x, y = v1
+# print(x, y)
+# print(v1)
+#
+# v1_clone = eval(str(v1))
+# print(v1_clone == v1)
+#
+# print(abs(v1))
+#
+# print(bool(v1))
