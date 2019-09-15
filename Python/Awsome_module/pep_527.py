4518d01f8f45c44b4003f3cc49236c1dfeeba946
qixiangyang <qixiangyangrm@foxmail.com>
2019-09-15 12:35:33 +0800

    海象运算符 PEP 572

---
+++ Python/Awsome_module/pep_527.py
@@ -0,0 +1,27 @@
+"""
+Description:
+Author:qxy
+Date: 2019-09-15 12:27
+File: pep_527 
+"""
+
+"""
+海象运算符
+参见https://www.dongwm.com/post/140/
+导致Guido大神累觉不爱的的提案
+
+释义：
+PEP572 的标题是「Assignment Expressions」，
+也就是「赋值表达式」，也叫做「命名表达式」，
+不过它现在被广泛的别名是「海象运算符」(The Walrus Operator)。
+因为:=很像海象「眼睛小，长着两枚长长的牙」这个特点 ^_^。
+from： https://www.dongwm.com/post/140/
+
+语法和语义
+:=
+
+自己的理解，可以一个计算过程赋值给一个变量，在一些需要判断的地方能够简化代码逻辑，优化代码结构
+
+"""
+
+
