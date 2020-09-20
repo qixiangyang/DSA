8341f52647583a49dcae49a217ae55df25fab259
xiangyang.qi <qixianyangrm@foxmail.com>
2020-09-20 14:29:28 +0800

    update

--- Algorithm/recursion.py
+++ Algorithm/recursion.py
@@ -5,6 +5,8 @@ Date: 2019-06-04 15:53
 File: digui 
 """
 from typing import List
+from functools import lru_cache
+import time
 
 # # 阶乘
 # def fact(n):
@@ -37,12 +39,31 @@ from typing import List
 
 # 斐波拉契数列
 
-def fib_list(n):
+# def fib_list(n):
+#     if n == 0:
+#         return 0
+#     if n < 3:
+#         return 1
+#     return fib_list(n-1) + fib_list(n-2)
+
+
+@lru_cache(maxsize=512)
+def fib_list_cached(n):
     if n == 0:
         return 0
     if n < 3:
         return 1
-    return fib_list(n-1) + fib_list(n-2)
+    return fib_list_cached(n-1) + fib_list_cached(n-2)
 
 
-print(fib_list(10))
+num = 500
+
+t1 = time.time()
+# print(fib_list(num))
+t2 = time.time()
+print(f"fib_list(50) cost {str(t2-t1)}")
+
+print(fib_list_cached(num))
+t3 = time.time()
+print(f"fib_list(50) cost {str(t3-t2)}")
+
