380baa10ae6a8264335904d429117c1c5ca20a7e
xiangyang.qi <xiangyang.qi@advancegroup.com>
2022-10-27 22:37:42 +0800

    add two sum result

--- Leetcode/00001_two_sum.py
+++ Leetcode/00001_two_sum.py
@@ -1,55 +1,78 @@
 """
-题目：
-给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
-你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
-示例:
+You can return the answer in any order.
 
-给定 nums = [2, 7, 11, 15], target = 9
+Example 1:
 
-因为 nums[0] + nums[1] = 2 + 7 = 9
-所以返回 [0, 1]
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+
+
+Constraints:
+
+2 <= nums.length <= 104
+-109 <= nums[i] <= 109
+-109 <= target <= 109
+Only one valid answer exists.
+
+
+Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
 """
 
+from typing import List
 
-# 暴力计算
-def twoSum(nums, target):
+
+def two_sum3(nums, target):
     """
-    :type nums: List[int]
-    :type target: int
-    :rtype: List[int]
+    key problem: must one in previous and another one is behind
+    use a temporary dict save left value and its index
     """
-    data_len = len(nums)
-
-    for i, num in enumerate(nums):
-        for m in range(i+1, data_len):
-            temp = nums[i] + nums[m]
-            if temp == target:
-                return [i+1, m+1]
-
-
-nums = [2, 7, 11, 15]
-target = 9
-
-data = twoSum(nums, target)
-print(data)
-
-
-# hash
-def twoSum2(nums, target):
-
     temp_dict = {}
-
     for i, v in enumerate(nums):
         left = target - v
-        if v in temp_dict.keys():
+        if v in temp_dict:
             return [temp_dict[v], i]
         temp_dict[left] = i
-        print(temp_dict)
     return []
 
 
-nums = [1, 2, 3, 5]
-data = twoSum2(nums, 6)
-print(data)
+def two_sum(nums: List[int], target: int) -> List[int]:
+    for index, value in enumerate(nums):
+        left = target - value
+        for sub_index, sub_value in enumerate(nums[index+1:]):
+            if left == sub_value:
+                return [index, index+sub_index+1]
+    return []
+
+
+def two_sum2(nums: List[int], target: int) -> List[int]:
+    nums_nums = len(nums)
+    for index, value in enumerate(nums):
+        for i in range(index+1, nums_nums):
+            if target - nums[index] - nums[i] == 0:
+                return [index, i]
+    return []
+
+
+if __name__ == '__main__':
+    Nums1 = [2, 7, 11, 15]
+    Target1 = 9
+
+    Nums2 = [3, 2, 4]
+    Target2 = 6
+
+    # data = two_sum2(Nums2, Target2)
+    data = two_sum3(Nums1, Target1)
+    print(data)
